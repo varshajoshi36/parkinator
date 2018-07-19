961a546b21a54e5389b2657eefcce110aaaab6b4
Geetakshi Batra <gbatra@gbatra-a01.vmware.com>
2018-07-19 11:52:14 -0700

    add remove garage

--- docker/webserver/app.py
+++ docker/webserver/app.py
@@ -12,6 +12,7 @@ api = Api(app)
 # Declare key_value store
 garage_DB = {}
 
+
 # Return parking garage details
 @app.route("/read", methods=['GET'])
 def read():
@@ -36,7 +37,6 @@ def read():
 # Add cars to garage and level
 @app.route("/add", methods=['POST'])
 def add():
-
     try:
         req = request.get_json()
         garage_name = req['garage']
@@ -51,6 +51,7 @@ def add():
 
     except:
         return "Fail"
+
 
 # Remove cars from garage and level
 @app.route("/remove", methods=['POST'])
@@ -78,20 +79,21 @@ def addGarage():
         req = request.get_json()
         garage_name = req['name']
         levelarr = req['lvlarr']
-        
+
         # add data
         garage_DB[garage_name] = {}
         garage_DB[garage_name]["name"] = garage_name
         garage_DB[garage_name]["lvlarr"] = [[levelNumber, total, total] for levelNumber, total in levelarr]
         total = 0
-        for _,levelSpace in levelarr:
-            total+=levelSpace
+        for _, levelSpace in levelarr:
+            total += levelSpace
         garage_DB[garage_name]["total"] = total
         garage_DB[garage_name]["occupied"] = 0
         garage_DB[garage_name]["available"] = total
         return jsonify(garage_DB)
     except:
         return "Fail"
+
 
 # Remove garage
 @app.route("/removegarage", methods=['POST'])
@@ -100,14 +102,15 @@ def removeGarage():
         # get and parse json
         req = request.get_json()
         garage_name = req['name']
-        
+
         if not garage_DB.has_key(garage_name):
             return "Garage doesn't exist. Bad request."
-        
+
         garage_DB.pop(garage_name, None)
         return jsonify(garage_DB)
     except:
         return "Fail"
 
+
 if __name__ == '__main__':
-	app.run(port=8080)
+    app.run(port=8080)
